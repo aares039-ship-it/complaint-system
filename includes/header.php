ae21728aa6ee3df548b1ec69d828e460389945e5
aares039-ship-it <aares039@gmail.com>
2026-02-15 19:15:07 +0200

    premiere  version

---
+++ includes/header.php
@@ -0,0 +1,5 @@
+<div class="header">
+    <h1>Complaint Management System</h1>
+    <p>Welcome, <?php echo htmlspecialchars($_SESSION['user_name']); ?> | Role: <?php echo $_SESSION['role']; ?></p>
+    <hr>
+</div>
