564e79e3ace80fad2b000834fb96dbec52b6a274
aares039-ship-it <aares039@gmail.com>
2026-03-22 21:05:28 +0200

    Delete includes directory

--- includes/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="footer">
-    <hr>
-    <p>&copy; <?php echo date("Y"); ?> Complaint Management System</p>
-</div>
--- includes/header.php
+++
@@ -1,5 +0,0 @@
-<div class="header">
-    <h1>Complaint Management System</h1>
-    <p>Welcome, <?php echo htmlspecialchars($_SESSION['user_name']); ?> | Role: <?php echo $_SESSION['role']; ?></p>
-    <hr>
-</div>
